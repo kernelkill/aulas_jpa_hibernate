5dadc6ccdd9fd1fc1e2b51103be69f3a9b4893d4
Joabe Kachorroski <kernelkill@outlook.com>
2016-06-27 23:36:53 -0400

    Criando telas para o Fabricante.

--- locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/service/CadastroFabricanteService.java
+++ locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/service/CadastroFabricanteService.java
@@ -2,12 +2,23 @@ package com.algaworks.curso.jpa2.service;
 
 import java.io.Serializable;
 
+import javax.inject.Inject;
+
+import com.algaworks.curso.jpa2.dao.FabricanteDAO;
 import com.algaworks.curso.jpa2.modelo.Fabricante;
 
 public class CadastroFabricanteService implements Serializable{
 	
+	@Inject
+	private FabricanteDAO fabricanteDAO;
+	
 	public void salvar(Fabricante fabricante) throws NegocioException{
 		
+		if (fabricante.getNome() == null || fabricante.getNome().trim().equals("")) {
+			throw new NegocioException("O nome do fabricante é obrigatorio!");
+		}
+		
+		this.fabricanteDAO.salvar(fabricante);
 	}
 
 }
