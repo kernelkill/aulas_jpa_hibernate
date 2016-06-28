570ecb5c344898d313e49071dc4a6d09e8f8c0b5
Joabe Kachorroski <kernelkill@outlook.com>
2016-06-27 23:13:33 -0400

    Criando as classes de DAO, Service e Exception.

---
+++ locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/service/CadastroFabricanteService.java
@@ -0,0 +1,13 @@
+package com.algaworks.curso.jpa2.service;
+
+import java.io.Serializable;
+
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+
+public class CadastroFabricanteService implements Serializable{
+	
+	public void salvar(Fabricante fabricante) throws NegocioException{
+		
+	}
+
+}
