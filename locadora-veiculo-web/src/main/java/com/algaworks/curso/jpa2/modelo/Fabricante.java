1471defa79a0d4dd62b68622977ef318524d9a91
Joabe Kachorroski <kernelkill@outlook.com>
2016-06-27 23:04:01 -0400

    Criando as camadas" de Modelos.

---
+++ locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -0,0 +1,35 @@
+package com.algaworks.curso.jpa2.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fabricante {
+	
+	
+	private Long codigo;
+	private String nome;
+	
+	//Getter and Setter
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
