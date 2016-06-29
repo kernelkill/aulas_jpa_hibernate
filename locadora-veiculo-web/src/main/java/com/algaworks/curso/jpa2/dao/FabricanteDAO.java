ea0b19c6a7b1c1642212e60ea486e8f416964588
Joabe Kachorroski <kernelkill@outlook.com>
2016-06-29 17:06:14 -0400

    Criando a area" de pesquisa de fabricante.

--- locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,6 +1,7 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -8,12 +9,17 @@ import javax.persistence.EntityManager;
 import com.algaworks.curso.jpa2.modelo.Fabricante;
 
 public class FabricanteDAO implements Serializable {
-	
+
 	@Inject
 	private EntityManager em;
-	
-	public void salvar(Fabricante fabricante){
-			em.persist(fabricante);
-			
+
+	public void salvar(Fabricante fabricante) {
+		em.persist(fabricante);
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return em.createQuery("from Fabricante").getResultList();
 	}
 }
